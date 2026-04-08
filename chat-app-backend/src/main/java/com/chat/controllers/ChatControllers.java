1abe8ac494e9110fcb83127115cf455190710c37
harshitkkush <harshitkkush@gmail.com>
2026-04-08 17:24:08 +0530

    describe what you changed

---
+++ chat-app-backend/src/main/java/com/chat/controllers/ChatControllers.java
@@ -0,0 +1,49 @@
+package com.chat.controllers;
+
+import java.time.LocalDateTime;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.chat.entities.Message;
+import com.chat.entities.Room;
+import com.chat.payload.MessageRequest;
+import com.chat.repositories.RoomRepository;
+
+@Controller
+@CrossOrigin("http://localhost:5173")
+public class ChatControllers {
+
+    private RoomRepository roomRepository;
+
+    public ChatControllers(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    // for sending and receiving messages
+    @MessageMapping("/sendMessage/{roomId}")
+    @SendTo("/topic/room/{roomId}")
+    public Message sendMessage(
+            @DestinationVariable String roomId,
+            @RequestBody MessageRequest request) {
+
+        Room room = roomRepository.findByRoomId(request.getRoomId());
+        Message message = new Message();
+        message.setContent(request.getContent());
+        message.setSender(request.getSender());
+        message.setTimeStamp(LocalDateTime.now());
+        if (room != null) {
+            room.getMessages().add(message);
+            roomRepository.save(room);
+        } else {
+            throw new RuntimeException("room not found !!");
+        }
+
+        return message;
+    }
+
+}
